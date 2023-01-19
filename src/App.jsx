428fc9a67b8bcd675285055285131c6c581ea469
Jed <prernakumari480@gmail.com>
2023-01-19 15:55:20 +0530

    created search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import { Input, Button } from 'reactstrap';
 import './App.css';
 
 export default function App() {
+  const [query, setQuery] = useState("");
+
   return (
     <div>
       <div className='navBar'>
         <img className='logoImage' width="50" src='movie-icon.webp' alt='logoImage' />
         <p className='title'> MoviesDB Search</p>
+      </div>
+      <div className='searchBar'>
+        <Input
+          className="search"
+          placeholder="Search movies and tv shows..."
+          onChange={e => setQuery(e.target.value)}
+          value={query} />
+        <Button className="search-btn" color="primary">
+          Search
+        </Button>
       </div>
     </div>
   );
