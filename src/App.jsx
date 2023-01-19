9b8479a6d1bef6925ac584a31b314d4dd3254cc9
Jed <prernakumari480@gmail.com>
2023-01-19 15:53:09 +0530

    implemented navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <div className='navBar'>
+        <img className='logoImage' width="50" src='movie-icon.webp' alt='logoImage' />
+        <p className='title'> MoviesDB Search</p>
+      </div>
+    </div>
+  );
+}
